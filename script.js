d417f00e94589c516b149cb11f6cc2ace2aef0ca
Hyuma Umeda <hyumaumeda@gmail.com>
2023-07-26 20:41:56 -0400

    We had a rudimentary rock paper scissors game. We revised it, so that there are click functions through JS. I also added a UI which has a title card, current score, display for each players move, and finally a win or lose alert function.

--- script.js
+++ script.js
@@ -31,54 +31,132 @@ function playRound (playerSelection, computerSelection) {
     }
 }
 
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    let result = playRound(prompt("Play Rock, Paper, Scissors. Choose one and type it below!"), getComputerChoice());
-    console.log(result);
-    if (result.slice(0,7) == "You Win"){
+const btn1 = document.querySelector(".rock");
+const btn2 = document.querySelector(".paper");
+const btn3 = document.querySelector(".scissors");
+const results = document.querySelector(".results");
+const playerScoreText = document.querySelector(".playerScore");
+const computerScoreText = document.querySelector(".computerScore");
+const playerChoiceImg = document.querySelector(".playerChoiceImg");
+const computerChoiceImg = document.querySelector(".computerChoiceImg");
+let playerScore = 0;
+let computerScore = 0;
+
+
+btn1.addEventListener("click", () => {
+    const compChoice = getComputerChoice();
+    results.textContent = (playRound("rock", compChoice));
+    playerChoiceImg.textContent = "ROCK";
+    computerChoiceImg.textContent = compChoice.toUpperCase();
+
+    if (results.textContent.slice(0,7) == "You Win"){
         playerScore += 1;
-    } else if (result.slice(0,8) == "You Lose"){
+    } else if (results.textContent.slice(0,8) == "You Lose"){
         computerScore += 1;
-    } 
-    result = playRound(prompt("Play Rock, Paper, Scissors. Choose one and type it below!"), getComputerChoice());
-    console.log(result);
-    if (result.slice(0,7) == "You Win"){
-        playerScore += 1;
-    } else if (result.slice(0,8) == "You Lose"){
-        computerScore += 1;
-    } 
-    result = playRound(prompt("Play Rock, Paper, Scissors. Choose one and type it below!"), getComputerChoice());
-    console.log(result);
-    if (result.slice(0,7) == "You Win"){
-        playerScore += 1;
-    } else if (result.slice(0,8) == "You Lose"){
-        computerScore += 1;
-    } 
-    result = playRound(prompt("Play Rock, Paper, Scissors. Choose one and type it below!"), getComputerChoice());
-    console.log(result);
-    if (result.slice(0,7) == "You Win"){
-        playerScore += 1;
-    } else if (result.slice(0,8) == "You Lose"){
-        computerScore += 1;
-    } 
-    result = playRound(prompt("Play Rock, Paper, Scissors. Choose one and type it below!"), getComputerChoice());
-    console.log(result);
-    if (result.slice(0,7) == "You Win"){
-        playerScore += 1;
-    } else if (result.slice(0,8) == "You Lose"){
-        computerScore += 1;
-    } 
-    if (playerScore >= computerScore){
-        return "You Win!";
-    } else if (playerScore <= computerScore){
-        return "You Lose!";
-    } else if (playerSCore == computerScore){
-        return "It's a tie!";
     }
-}
-console.log(game());
+    playerScoreText.textContent = playerScore;
+    computerScoreText.textContent = computerScore;
+
+    if (playerScore == 5) {
+        alert("Winner Winner Chicken Dinner!");
+    } else if (computerScore == 5) {
+        alert("Come by again, loser!");
+    }
+});
+btn2.addEventListener("click", () => {
+    const compChoice = getComputerChoice();
+    results.textContent = (playRound("paper", compChoice));
+    playerChoiceImg.textContent = "PAPER";
+    computerChoiceImg.textContent = compChoice.toUpperCase();
+
+    if (results.textContent.slice(0,7) == "You Win"){
+        playerScore += 1;
+    } else if (results.textContent.slice(0,8) == "You Lose"){
+        computerScore += 1;
+    }
+    playerScoreText.textContent = playerScore;
+    computerScoreText.textContent = computerScore;
+
+    if (playerScore == 5) {
+        alert("Winner Winner Chicken Dinner!");
+    } else if (computerScore == 5) {
+        alert("Come by again, loser!");
+    }
+})
+;
+btn3.addEventListener("click", () => {
+    const compChoice = getComputerChoice();
+    results.textContent = (playRound("scissors", compChoice));
+    playerChoiceImg.textContent = "SCISSORS";
+    computerChoiceImg.textContent = compChoice.toUpperCase();
+
+    if (results.textContent.slice(0,7) == "You Win"){
+        playerScore += 1;
+    } else if (results.textContent.slice(0,8) == "You Lose"){
+        computerScore += 1;
+    }
+    playerScoreText.textContent = playerScore;
+    computerScoreText.textContent = computerScore;
+
+    if (playerScore == 5) {
+        alert("Winner Winner Chicken Dinner!");
+    } else if (computerScore == 5) {
+        alert("Come by again, loser!");
+    }
+});
+
+
+// function game() {
+//     let playerScore = 0;
+//     let computerScore = 0;
+//     let result = playRound(prompt("Play Rock, Paper, Scissors. Choose one and type it below!"), getComputerChoice());
+//     console.log(result);
+//     if (result.slice(0,7) == "You Win"){
+//         playerScore += 1;
+//     } else if (result.slice(0,8) == "You Lose"){
+//         computerScore += 1;
+//     } 
+//     result = playRound(prompt("Play Rock, Paper, Scissors. Choose one and type it below!"), getComputerChoice());
+//     console.log(result);
+//     if (result.slice(0,7) == "You Win"){
+//         playerScore += 1;
+//     } else if (result.slice(0,8) == "You Lose"){
+//         computerScore += 1;
+//     } 
+//     result = playRound(prompt("Play Rock, Paper, Scissors. Choose one and type it below!"), getComputerChoice());
+//     console.log(result);
+//     if (result.slice(0,7) == "You Win"){
+//         playerScore += 1;
+//     } else if (result.slice(0,8) == "You Lose"){
+//         computerScore += 1;
+//     } 
+//     result = playRound(prompt("Play Rock, Paper, Scissors. Choose one and type it below!"), getComputerChoice());
+//     console.log(result);
+//     if (result.slice(0,7) == "You Win"){
+//         playerScore += 1;
+//     } else if (result.slice(0,8) == "You Lose"){
+//         computerScore += 1;
+//     } 
+//     result = playRound(prompt("Play Rock, Paper, Scissors. Choose one and type it below!"), getComputerChoice());
+//     console.log(result);
+//     if (result.slice(0,7) == "You Win"){
+//         playerScore += 1;
+//     } else if (result.slice(0,8) == "You Lose"){
+//         computerScore += 1;
+//     } 
+//     if (playerScore >= computerScore){
+//         return "You Win!";
+//     } else if (playerScore <= computerScore){
+//         return "You Lose!";
+//     } else if (playerSCore == computerScore){
+//         return "It's a tie!";
+//     }
+// }
+// console.log(game());
+
+
+
 // const playerSelection = "RocK";
-// const computerSelection = getComputerChoice();
+// const computerSelection = "Scissors";
 // console.log(computerSelection)
 // console.log(playRound(playerSelection, computerSelection));
